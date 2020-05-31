59ee8e604c3934c97291382c12456e824c5cf928
Romain Guyot <romain@solinkcorp.com>
2020-05-31 16:26:06 -0400

    Update TODO

--- src/utils/version.ts
+++ src/utils/version.ts
@@ -1,6 +1,13 @@
 /**
  * If the following works, then we might have a working version injection
+<<<<<<< Updated upstream
  * Version: 0.0.1 - built on {Sun May 31 2020 11:50:54 GMT-0400 (Eastern Daylight Time)}
  */
 
 export const VERSION = "Version: 0.0.1 - built on Sun May 31 2020 11:50:54 GMT-0400 (Eastern Daylight Time)";
+=======
+ * Version: 0.0.1 - built on {Sat May 30 2020 14:03:10 GMT-0400 (Eastern Daylight Time)}
+ */
+
+export const VERSION = "Version: 0.0.1 - built on Sat May 30 2020 14:03:10 GMT-0400 (Eastern Daylight Time)";
+>>>>>>> Stashed changes
